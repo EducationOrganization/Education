afc505dd83473a41740137205edcb5650710fbcb
KosovAnton <kosov.business@gmail.com>
2013-12-12 12:52:57 +0200

    Add Persone class. Realize persones sort.

--- src/sortCSVFileByColumns/Collect.java
+++ src/sortCSVFileByColumns/Collect.java
@@ -1,28 +1,53 @@
 package sortCSVFileByColumns;
 
 import java.io.*;
-import java.util.ArrayList;
 import java.util.*;
 
 public class Collect {
 	
-	static File csvSource = null;
-	static BufferedReader unsortedFile = null;
+	static File source = null;
+	static BufferedReader in = null;
+	static PrintWriter out = null;
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException{	
+		//Collection that contains Persone objects.
+		List<Persone> persones = new ArrayList<Persone>();
 		
-		csvSource = new File("source.txt");
-		unsortedFile = new BufferedReader(
-										  new FileReader(csvSource));
-		
-		List<String> list = new ArrayList<String>();
-				
-		String s ="";
-		
-		while((s=unsortedFile.readLine()) != null){
-			list.add(s);
+		try{
+			//Read CSV file.
+			in = new BufferedReader(
+					 new FileReader("source.txt"));
+			
+			//File to write sorted CSV object.
+			out = new PrintWriter(
+					  new BufferedWriter(
+						  new FileWriter("D:\\sortedCSV.txt")));
+			
+			//1.Read by lines, 
+			//2.Split to array by comas.
+			//3.Fill persones object with array elements. 
+			String s;
+			Persone persone;
+			while((s=in.readLine()) != null){
+				String [] personalData = s.split(",");
+				persone = new Persone(personalData[0], personalData[1], personalData[2], personalData[3], personalData[4]);
+				persones.add(persone);
+			}
+			
+			Collections.sort(persones);
+			
+			for(Persone p : persones){
+				System.out.println(p);
+				out.println(p);
+			}
+		}catch(IOException e){
+			e.printStackTrace();
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			in.close();
+			out.close();
 		}
-		Collections.sort(list);
-		System.out.println(list);
+		
 	}
 }
---
+++ src/sortCSVFileByColumns/Persone.java
@@ -0,0 +1,64 @@
+package sortCSVFileByColumns;
+
+public class Persone implements Comparable<Persone>{
+	private String name, emailAddress, firstName, lastName,password;
+	
+	Persone (String name, String emailAddress, String firstName, String lastName, String password){
+		this.setName(name);
+		this.setEmailAddress(emailAddress);
+		this.setFirstName(firstName);
+		this.setLastName(lastName);
+		this.setPassword(password);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password2) {
+		this.password = password2;
+	}
+	
+	public String toString(){
+		return name +" "+ emailAddress +" "+ firstName + " " + lastName + " " + password;
+	}
+
+	@Override
+	public int compareTo(Persone o) {
+		return this.name.compareTo(o.name);
+	}
+	
+
+}
