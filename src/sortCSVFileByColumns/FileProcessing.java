69fb1024a67fd26c76ac11343519400ffe42f0c1
KosovAnton <kosov.business@gmail.com>
2013-12-13 17:07:38 +0200

    Solved.

--- src/sortCSVFileByColumns/FileProcessing.java
+++ src/sortCSVFileByColumns/FileProcessing.java
@@ -3,42 +3,38 @@ package sortCSVFileByColumns;
 import java.io.*;
 import java.util.*;
 
-public class Collect {
+public class FileProcessing {
 	
 	static File source = null;
 	static BufferedReader in = null;
 	static PrintWriter out = null;
 	
-	public static void main(String[] args) throws IOException{	
-		//Collection that contains Persone objects.
+	public static void readAndSort() throws IOException{	
+		//1.This collection contains 'Persone' class objects to be sorted.
 		List<Persone> persones = new ArrayList<Persone>();
 		
 		try{
-			//Read CSV file.
+			//2..Get access to general csv file
 			in = new BufferedReader(
 					 new FileReader("source.txt"));
 			
-			//File to write sorted CSV object.
+			//3.Create file to save sorted csv results.
 			out = new PrintWriter(
 					  new BufferedWriter(
 						  new FileWriter("D:\\sortedCSV.txt")));
 			
-			//1.Read by lines, 
-			//2.Split to array by comas.
-			//3.Fill persones object with array elements. 
+			//4.Create 'Persone' objects by csv file lines and add them to collection 'persones'. 
 			String s;
-			Persone persone;
 			while((s=in.readLine()) != null){
 				String [] personalData = s.split(",");
-				persone = new Persone(personalData[0], personalData[1], personalData[2], personalData[3], personalData[4]);
+				Persone persone = new Persone(personalData[0], personalData[1], personalData[2], personalData[3], personalData[4]);
 				persones.add(persone);
 			}
-			//Persone.sortByLastName();
-			//Persone.sortByEmailAddress();
-			//Persone.sortByFirstName();
-			//Persone.sortByPassword();
+			
+			//5.Sort collection of persones.
 			Collections.sort(persones);
 			
+			//6.Delete a row with column names and place it on the first line.
 			Iterator<Persone> itr = persones.iterator();
 			while(itr.hasNext()){
 				if(itr.next().getName().equals("Name")){
@@ -46,6 +42,8 @@ public class Collect {
 				}
 			}
 			persones.add(0, new Persone("Name", "EmailAddress", "FirstName", "LastName", "Password"));
+			
+			//7.Display sorting results on the console and write it to file.
 			for(Persone p : persones){
 				System.out.println(p);
 				out.println(p);
@@ -57,7 +55,6 @@ public class Collect {
 		}finally{
 			in.close();
 			out.close();
-		}
-		
+		}		
 	}
 }
---
+++ src/sortCSVFileByColumns/StartSorting.java
@@ -0,0 +1,20 @@
+package sortCSVFileByColumns;
+
+import java.io.IOException;
+
+public class StartSorting {
+	public static void main(String[] args) {
+		//Default sorting will procesed by column 'Name'.
+		//To change sorting type remove comment sign '//'.
+		//The following methods specify the column that will be made sorting:
+			//Persone.sortByFirstName();
+			//Persone.sortByLastName();
+			//Persone.sortByEmailAddress();
+			//Persone.sortByPassword();
+		try {
+			FileProcessing.readAndSort();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
