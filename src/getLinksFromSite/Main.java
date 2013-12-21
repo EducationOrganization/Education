9dd325d1666da2d72d5a5349a0a946c0dc7c396a
KosovAnton <kosov.business@gmail.com>
2013-12-22 01:35:17 +0200

    Some additions

--- src/getLinksFromSite/Main.java
+++ src/getLinksFromSite/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 public class Main {
 	public static void main(String[] args) {		
 		try{
+			//dfgdgfdg
 			ReadContent.readFile();
 			WriteURLs.write();
 		}catch(IOException e){
